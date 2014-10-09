09b0f3fc7efe6e15f78d0fc60fc231bd503e11c2
suicide <suicide@get-it.us>
2014-10-09 02:37:12 +0200

    correct solution for problem 32

--- src/main/scala/euler/pandigitalproducts/PandigitalProducts.scala
+++ src/main/scala/euler/pandigitalproducts/PandigitalProducts.scala
@@ -7,4 +7,34 @@ package euler.pandigitalproducts
  */
 object PandigitalProducts extends App {
 
+  def isPandigitalProduct(x: Int, y: Int): Boolean = {
+
+    def digits(number: Int): List[Int] = {
+      if (number < 10) {
+        List(number)
+      } else {
+        number % 10 :: digits(number / 10)
+      }
+    }
+
+    def isPandigital(digits: Seq[Int]): Boolean = digits.size.equals(9) && digits.toSet.size.equals(9) && !digits.contains(0)
+
+    // duh
+    if (x.equals(y)) {
+      return false
+    }
+
+    val product = x * y
+
+    isPandigital( digits(x) ++ digits(y) ++ digits(product))
+  }
+
+  val products = for {
+    x <- 1 to 1000
+    y <- 1 to 10000
+    if isPandigitalProduct(x, y)
+  } yield x.toLong * y.toLong
+
+  println(products.toSet.sum)
+
 }
